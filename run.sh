8eb5bb39167f6f6a6b76ef84d813c1f565bc8f28
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-09-11 15:13:21 +0600

    + Check is root option. Check is command passed

--- run.sh
+++ run.sh
@@ -13,6 +13,47 @@ DOWNLOAD_FOLDER="${SCRIPT_PATH}/downloads"
 DOWNLOAD_TARGET="${DOWNLOAD_FOLDER}/${ROOT_HINT_LOCAL_FILE}"
 REMOTE_SIZE=$(curl -sI "${ROOT_HINT_URL}" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
 
+# Arguments
+function usage() {
+    echo -e "Usage: $0 [-h] \n"
+}
+
+function check_root() {
+    if [ "$EUID" -ne 0 ]; then
+        echo "Please run as root"
+        exit 1
+    fi
+}
+
+# Checks passed arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -r|--root) _ROOT=1; ;;
+        -c|--command) _COMMAND=1 _COMMAND_DATA=$2; shift ;;
+        -h|--help) usage ;; 
+        # *) usage ;;
+    esac
+    shift
+done
+
+# Check if user is root
+if [ ! -z ${_ROOT} ]; then
+    check_root
+fi
+
+# Check passed command
+if [ ${_COMMAND} -eq 1 ]; then
+    if [ -z ${_COMMAND_DATA} ]; then
+        echo "No command passed"
+        exit 1
+    fi
+    echo "${_COMMAND_DATA}"
+    exit 0
+fi
+
+
+# Script routines
+
 # Check if download folder exist
 if [ ! -d "${DOWNLOAD_FOLDER}" ]; then
   mkdir -p "${DOWNLOAD_FOLDER}"
